ac39493402ad669c07e3c3ad8f2ecf8ce739d9b9
hfawson <heidifawson@gmail.com>
2018-09-04 09:55:48 -0600

    passed off (:

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -8,11 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var celsiusLabel: UILabel!
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let f = self.fahrenheitValue {
+            return f.converted(to: .celsius)
+        }
+        else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let c = celsiusValue {
+            self.celsiusLabel.text = numberFormatter.string(from: NSNumber(value: c.value))
+        }
+        else {
+            self.celsiusLabel.text="???"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateCelsiusLabel()
+        print("conversion controller loaded")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +56,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func dissmissKeyboard(_ sender: UITapGestureRecognizer) {
+        self.textField.resignFirstResponder()
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ sender: UITextField) {
+        if let text = textField.text, let v = Double(text) {
+            self.fahrenheitValue = Measurement(value: v, unit: .fahrenheit)
+        }
+        else{
+            self.fahrenheitValue = nil
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        //print("Replacement: \(string)")
+        //print("Current: \(textField.text)")
+        
+        let existingDecimal = textField.text?.range(of: ".")
+        let replacementDecimal = string.range(of: ".")
+        
+        if existingDecimal != nil && replacementDecimal != nil {
+            return false
+        } else {
+            return true
+        }
+    }
 }
 
